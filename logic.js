15f4cfb648f625a1f4bf6ac7ccf80678fbdaee39
Christian Grimm <grimmchristian@gmx.de>
2024-09-28 14:29:28 +0200

    Finish the prototype
    finished a running version of the app. Also, started to implement first
    feature: refreshing and rezising the board. Also, putted all the css
    into the js to have more controll over dynamics.

--- logic.js
+++ logic.js
@@ -1,29 +1,79 @@
-// Registrate globals and starting-conditions 
+// Registrate globals and starting-conditions
 const initialize = () => {
     const initialValues = {
         startSize: 16,
-        container: document.querySelector('#container')
-    }
+        container: document.querySelector("#container"),
+        newField: document.querySelector("#newField"),
+    };
     return initialValues;
-}
+};
+
+const addCSS = () => {
+    const style = document.createElement("style");
+    style.innerHTML = `
+        body {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            gap: 10px;
+        }
+
+        #container {
+            border: 1px solid black;
+            width: 700px;
+            height: 700px;
+            display: flex;
+            flex-wrap: wrap;
+        }
+    `;
+
+    document.head.appendChild(style);
+};
 
 // Creating the board as a flex-layout with given squares
-const createBoard = (size) => {
+const createBoard = (size, container) => {
     const totalSquares = size * size;
 
     for (let i = 0; i < totalSquares; i++) {
-        createSquare();
+        createSquare(size, container, i);
     }
 };
 
 // Creating a single square with neccessary attributes
-const createSquare = () => {
-    const square = document.createElement("div")
+const createSquare = (size, container, number) => {
+    const square = document.createElement("div");
     square.className = "squareTest";
-    container.appendChild(square);
-    console.log("1");
-}
+    square.id = "sid" + number;
+    square.style.width = 700 / size + "px";
+    square.style.height = 700 / size + "px";
 
+    square.addEventListener("mouseenter", () => changeSquare(square));
+    container.appendChild(square);
+};
+
+// Effects for hovering the squares
+const changeSquare = (square) => {
+    square.style.backgroundColor = "black";
+};
+
+// The actions for the panel options
+const panelActions = (newField) => {
+    newField.addEventListener("click", () => {
+        let passed = false;
+        let userPrompt = 0;
+        do {
+            userPrompt = parseInt(prompt("Enter Dimensions for new field [1-100]"));
+            if (userPrompt > 0 && userPrompt <= 100) {
+                passed = true;
+            }
+        } while (!passed);
+        console.log(userPrompt);
+    })
+
+    return null;
+}
 // Here starts the main logic
-const { startSize, container } = initialize();
-createBoard(startSize);
+addCSS();
+const { startSize, container, newField } = initialize();
+createBoard(startSize, container);
+panelActions(newField);
